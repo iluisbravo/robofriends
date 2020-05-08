71c0011b9bd6c9c5c098d63bb317d399fd39995a
iluisbravotis <ing.luisalbertobravo@gmail.com>
2020-05-08 16:29:29 -0700

    add error boundry component

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflowY : 'scroll', border: '3px solid black', height: '600px' }}>
+        <div style={{ overflowY : 'scroll', border: '3px solid black', height: '600px', margin: '30px', paddingTop: '20px', paddingBottom: '20px' }}>
             {props.children}
         </div>
     );
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -49,10 +50,12 @@ class App extends Component {
 
         return(
             <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
+                <h1 className="f2">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                    {bodyApp}
+                    <ErrorBoundry>
+                       {bodyApp}
+                    </ErrorBoundry>               
                 </Scroll>                
             </div>
         );        
